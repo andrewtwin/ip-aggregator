45709922432e0ae4380d710e10ef2819434a4656
Andrew Twin <andrewtwin@gmail.com>
2021-12-12 09:29:31 +1100

    update version number

--- __main__.py
+++ __main__.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
-VERSION = "v0.6.2"
+VERSION = "v0.6.3"
 
 import ipaddress
 import argparse
